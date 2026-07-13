a216043e602d7a57cc02f014bd69ee8a946bda9e
RaghvPratap <raghvpratap5@gmail.com>
2026-07-13 12:42:24 +0530

    IOC Container & Bean

--- src/main/java/com/raghv/demo/DemoApplication.java
+++ src/main/java/com/raghv/demo/DemoApplication.java
@@ -2,12 +2,16 @@ package com.raghv.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		Student student = new Student("Mohan", 20);
+//		SpringApplication.run(DemoApplication.class, args);
+		ApplicationContext context= SpringApplication.run(DemoApplication.class,args);
+		Student student=context.getBean(Student.class);
+		student.setAge(20);
+		student.setName("Rohan");
 		System.out.println(student.getName());
 		System.out.println(student.getAge());
 	}
