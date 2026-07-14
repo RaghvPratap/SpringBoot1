34d091c33f28909aedceaee0b0a564f78a760b44
RaghvPratap <raghvpratap5@gmail.com>
2026-07-14 12:55:09 +0530

    Sending & Converting into JSON

--- src/main/java/com/raghv/demo/StudentServer/StudentServer.java
+++ src/main/java/com/raghv/demo/StudentServer/StudentServer.java
@@ -2,19 +2,19 @@ package com.raghv.demo.StudentServer;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StudentServer {
     //1.Store the student
     @PostMapping("/create")
-    public String storeStudent(){
-        return """
-                id=1,
-                name:"Raghv",
-                dept:"CSE",
-                age:22
-                """;
+    public String storeStudent(@RequestBody Student student){
+        int id= student.getId();
+        int age=student.getAge();
+        String name=student.getName();
+        String dept=student.getDept();
+        return "id:"+id+"age:"+age+"name:"+name+"dept:"+dept;
     }
     //2.Retrieve the student
     //3.Update the student
