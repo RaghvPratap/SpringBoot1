6090e73f5041ffd8a87b4678c6ab6992df9dd80b
RaghvPratap <raghvpratap5@gmail.com>
2026-07-17 14:48:24 +0530

    Get Student Data

--- src/main/java/com/raghv/demo/StudentServer/Controller/StudentController.java
+++ src/main/java/com/raghv/demo/StudentServer/Controller/StudentController.java
@@ -1,8 +1,10 @@
 package com.raghv.demo.StudentServer.Controller;
 
 import com.raghv.demo.StudentServer.Service.StudentService;
+import com.raghv.demo.StudentServer.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 //Responsible for hearing the incoming HTTP requests
@@ -14,12 +16,19 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-//    @PostMapping("/create")
-//    public ResponseEntity<Student> storeStudent(@RequestBody Student student){
-//        Student res=studentService.studentValidate(student);
-//        if(res==null){
-//            return ResponseEntity.status(400).body(res);
-//        }
-//        return ResponseEntity.status(201).body(res);
-//    }
+    @PostMapping("/create")
+    public ResponseEntity<Student> storeStudent(@RequestBody Student student){
+        Student res=studentService.studentValidate(student);
+        if(res==null){
+            return ResponseEntity.status(400).body(res);
+        }
+        return ResponseEntity.status(201).body(res);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<?> getStudentById(@PathVariable int id) {
+        Student student = studentService.getStudentById(id);
+        return ResponseEntity.status(200).body(student);
+    }
+
 }
--- src/main/java/com/raghv/demo/StudentServer/Service/StudentService.java
+++ src/main/java/com/raghv/demo/StudentServer/Service/StudentService.java
@@ -23,4 +23,8 @@ public class StudentService {
         studentRepository.save(student);
         return student;
     }
+
+    public Student getStudentById(int id) {
+        return studentRepository.findById(id).orElse(null);
+    }
 }
